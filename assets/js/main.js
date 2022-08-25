258925dbcca793bde52610966b45df655d4e67d5
helciocandido <helcio.mcandido@gmail.com>
2022-08-25 16:53:39 -0400

    Adicionado botão para desativar e ativar a música.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,29 @@ invertBtn.addEventListener('click', () => {
   const audio = document.getElementById('music');
   audio.src = `./assets/musics/${music}`;
   audio.play();
-  audio.volume = 0.1;
+  audio.volume = 0.2;
 });
 
-window.addEventListener('click', () => {
+window.addEventListener('DOMContentLoaded', () => {
   const audio = document.getElementById('music');
   audio.play();
-  audio.volume = 0.1;
+  audio.volume = 0.2;
+});
+
+const volumeBtn = document.getElementById('volume-btn');
+
+volumeBtn.addEventListener('click', () => {
+  const audio = document.getElementById('music');
+  const volumeUp = '<span class="material-symbols-outlined"> volume_up </span>';
+  const volumeOff =
+    '<span class="material-symbols-outlined"> volume_off </span>';
+  if (volumeBtn.innerHTML == volumeOff) {
+    audio.play();
+    audio.volume = 0;
+    volumeBtn.innerHTML = volumeUp;
+  } else {
+    volumeBtn.innerHTML = volumeOff;
+    audio.play();
+    audio.volume = 0.2;
+  }
 });
